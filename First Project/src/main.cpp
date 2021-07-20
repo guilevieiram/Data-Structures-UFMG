3496215e1dc0d58897e970988e66d4930213dbd4
HenrySilvaCS <trupidomidia@gmail.com>
2021-07-20 11:50:36 -0300

    updated docs

--- First Project/src/main.cpp
+++ First Project/src/main.cpp
@@ -12,8 +12,11 @@ int main(int argc, char *argv[]){
         std::cerr << "Error: Unable to open file!" << std::endl;
         return 0;
     }
-    unsigned int num_serv;
+    int num_serv;
     infile >> num_serv; // Lê o número de buffers da primeira linha do arquivo
+    if(num_serv <= 0){
+        return 0;
+    }
 
     Buffer<std::string> *servers = new Buffer<std::string>[num_serv]; // Inicializa o array de buffers
     Buffer<std::string> hist; // Inicializa o historico de consciencias enviadas
@@ -21,7 +24,7 @@ int main(int argc, char *argv[]){
     std::string txt; // Variável que irá armazenar todo o texto de uma dada linha do arquivo
     std::string delim = " ", token; // Variáveis auxiliares para o parsing das strings
     int pos = 0; // Variável auxiliar para o parsing das strings
-    unsigned int tgt_serv, dest_serv, serv_pos; // Variáveis auxiliares para execução de comandos
+    int tgt_serv, dest_serv, serv_pos; // Variáveis auxiliares para execução de comandos
 
     while(std::getline(infile,txt)){ // Loop while que lê cada linha do arquivo contendo os comandos
         if(txt.size() == 0){
@@ -56,7 +59,7 @@ int main(int argc, char *argv[]){
             if((tgt_serv >= num_serv) || (tgt_serv < 0)){
                 continue;
             }
-            else if((serv_pos >= (unsigned int)servers[tgt_serv].get_size()) || (serv_pos < 0)){
+            else if((serv_pos >= servers[tgt_serv].get_size()) || (serv_pos < 0)){
                 continue;
             }
             // Executa o comando
@@ -96,7 +99,7 @@ int main(int argc, char *argv[]){
         if(token == "SEND"){
             // Executa o comando
             Item<std::string> curr_item;
-            for(unsigned int i = 0; i < num_serv; i++){
+            for(int i = 0; i < num_serv; i++){
                 try{ // Caso o buffer esteja vazio, não remove seu primeiro item
                     curr_item = servers[i].pop_front();
                 }
@@ -109,7 +112,7 @@ int main(int argc, char *argv[]){
         if(token == "FLUSH"){
             // Executa o comando
             hist.print();
-            for(unsigned int i = 0; i < num_serv; i++){
+            for(int i = 0; i < num_serv; i++){
                 servers[i].print();
             }
         }
